7af0a40fe9b38c4549bbf0dd9001fbc2cf0ac985
Alya R <alyaramadhani000@gmail.com>
2021-03-29 20:13:45 +0700

    soal 1

---
+++ I0320123_Soal1_Tugas5.py
@@ -0,0 +1,7 @@
+nama = input("Nama saya")
+jenis_kelamin = input("Jenis kelamin saya adalah (L/P)")
+
+if (jenis_kelamin =='L'):
+    print("Selamat datang, Tuan", nama)
+else:
+    print("Selamat datang,  Nyonya", nama)
